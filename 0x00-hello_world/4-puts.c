1122d8384e42bb0239c6e99b149ece746c44f1e5
Earthfill <kingsley.omiy@gmail.com>
2022-12-08 18:05:03 +0100

    Task 4 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/*
- * main - print a ine of code using puts
+/**
+ * main -Print a line of code using puts
  *
  * Return: 0
 */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
