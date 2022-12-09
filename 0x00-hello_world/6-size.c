49383fe70f8b6c01d3f7abcd73c0097672588a01
Earthfill <kingsley.omiy@gmail.com>
2022-12-09 06:04:29 +0100

    Task 6 commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	long long int d;
 	float f;
 
-	printf("Size of a char: %lu byte(S)\n", sizeof(a));
-	printf("Size of an int: %lu byte(S)\n", sizeof(b));
-	printf("Size of a long int: %lu byte(S)\n", sizeof(c));
-	printf("Size of a long long int: %lu byte(S)\n", sizeof(d));
-	printf("Size of a float: %lu byte(S)\n", sizeof(f));
+	printf("Size of a char: %lu byte(S)\n", sizeof(char));
+	printf("Size of an int: %lu byte(S)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(S)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(S)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(S)\n", sizeof(float));
 	return (0);
 }
