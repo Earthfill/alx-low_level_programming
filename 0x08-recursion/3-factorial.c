f27123a079302cc96ab9a540a9e784143c2fa3d5
Earthfill <kingsley.omiy@gmail.com>
2023-01-04 12:22:30 +0100

    Task 3 commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+/*
+ * File: 3-factorial.c
+ * Auth: kingslee
+*/
+
+#include "main.h"
+
+/**
+ * factorial - Returns the factorial of a given number.
+ * @n: The number to find the factorial of.
+ *
+ * Return: factorial.
+*/
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+
+	else if (n < 0)
+		return (-1);
+	else
+		return (n * factorial(n - 1));
+}
